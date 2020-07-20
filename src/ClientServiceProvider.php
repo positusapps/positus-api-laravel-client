8ee8116c223a2e1fe9359995b3715bf703ced548
caiokawasaki <caiokawasaki@gmail.com>
2020-07-20 17:00:45 -0300

    singleton removed

--- src/ClientServiceProvider.php
+++ src/ClientServiceProvider.php
@@ -18,9 +18,5 @@ class ClientServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/positus.php', 'positus');
-
-        $this->app->singleton(Client::class, function () {
-            return new Client();
-        });
     }
 }
